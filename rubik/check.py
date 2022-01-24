b0c9d401bf86194291abfc5d32f192809f9152c3
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-01-24 09:45:39 -0600

    Not yet done

--- rubik/check.py
+++ rubik/check.py
@@ -4,7 +4,7 @@ def _check(parms):
     result={}
     encodedCube = parms.get('cube',None)       
     if(encodedCube == None):
-        result['status'] = 'error: xxx'
+        result['status'] = 'error: no cube found'
     else:
         result['status'] = 'a-ok'
     return result
