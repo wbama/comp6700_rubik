a500ee3723534753b3f2d8a4474ecfcd7ed95be7
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-22 12:04:25 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -308,21 +308,21 @@ def _solve(parms):
         
     elif inputDict['rotate'] == 'U':
         print(lst_cube)
-        side_0 = cube[4]
+        side_0 = lst_cube[4]
 
-        side_1 = cube[1]
+        side_1 = lst_cube[1]
         #rotate it correctly
         side_1 = (turn_type2(side_1))
-        side_2 = cube[5]
+        side_2 = lst_cube[5]
         #rotate side 2
         side_2 = (turn_type3(side_2))
-        side_3 = cube[3]
+        side_3 = lst_cube[3]
         #rotate side 3
         side_3 = (turn_type1(side_3))
         #rotate side 4
-        side_4 = cube[2]
+        side_4 = lst_cube[2]
         side_4 = (turn_type3(side_4))
-        side_5 = cube[0]        
+        side_5 = lst_cube[0]        
         cube = []
         for i in range(6):
             exec(f'cube.append(side_{i})') 
@@ -337,10 +337,28 @@ def _solve(parms):
         side_1 = cube[1]
         #rotate it correctly
         side_1 = (turn_type2(side_1))
-        side_2 = cube[5]
+        side_2 = lst_cube[5]
         #rotate side 2
         side_2 = (turn_type3(side_2))
-        side_3 = cube[3]
+        side_3 = lst_cube[3]
+        #rotate side 3
+        side_3 = (turn_type1(side_3))
+        #rotate side 4
+        side_4 = lst_cube[2]
+        side_4 = (turn_type3(side_4))
+        side_5 = lst_cube[0]        
+        cube = []
+        for i in range(6):
+            exec(f'cube.append(side_{i})') 
+        side_0 = cube[4]
+
+        side_1 = lst_cube[1]
+        #rotate it correctly
+        side_1 = (turn_type2(side_1))
+        side_2 = lst_cube[5]
+        #rotate side 2
+        side_2 = (turn_type3(side_2))
+        side_3 = lst_cube[3]
         #rotate side 3
         side_3 = (turn_type1(side_3))
         #rotate side 4
@@ -350,39 +368,21 @@ def _solve(parms):
         cube = []
         for i in range(6):
             exec(f'cube.append(side_{i})') 
-        side_0 = cube[4]
+        side_0 = lst_cube[4]
 
         side_1 = cube[1]
         #rotate it correctly
         side_1 = (turn_type2(side_1))
-        side_2 = cube[5]
+        side_2 = lst_cube[5]
         #rotate side 2
         side_2 = (turn_type3(side_2))
-        side_3 = cube[3]
+        side_3 = lst_cube[3]
         #rotate side 3
         side_3 = (turn_type1(side_3))
         #rotate side 4
-        side_4 = cube[2]
+        side_4 = lst_cube[2]
         side_4 = (turn_type3(side_4))
-        side_5 = cube[0]        
-        cube = []
-        for i in range(6):
-            exec(f'cube.append(side_{i})') 
-        side_0 = cube[4]
-
-        side_1 = cube[1]
-        #rotate it correctly
-        side_1 = (turn_type2(side_1))
-        side_2 = cube[5]
-        #rotate side 2
-        side_2 = (turn_type3(side_2))
-        side_3 = cube[3]
-        #rotate side 3
-        side_3 = (turn_type1(side_3))
-        #rotate side 4
-        side_4 = cube[2]
-        side_4 = (turn_type3(side_4))
-        side_5 = cube[0]        
+        side_5 = lst_cube[0]        
         cube = []
         for i in range(6):
             exec(f'cube.append(side_{i})') 
