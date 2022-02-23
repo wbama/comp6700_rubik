38622df2e87b9c2324d9d1da0a4b886768508dd3
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-23 17:25:59 -0600

    Green Light |

--- rubik/solveCheckInput.py
+++ rubik/solveCheckInput.py
@@ -49,26 +49,24 @@ def solveCheck(parms):
         
         for i in range (1,7):
             for j in range(1,10):
-                exec(f"lst_in{i}.append(lst_cube.pop(0))")  
-                
-        #create list within a list of the cube.   
-#        lst_cube = []
+                exec(f"lst_in{i}.append(lst_cube.pop(0))")                 
+
 
         for i in range(1,7):
-            exec(f'lst_cube.append(lst_in{i})')        
+            exec(f'lst_cube.append(lst_in{i})')     
   
          
         #create an instance of the cube by creating nested list        
-        lst_cube = ([x for x in parms.get("cube")])
-        
-        lst_in1, lst_in2, lst_in3, lst_in4, lst_in5, lst_in6 = ([] for i in range(6)) 
-        
-        for i in range (1,7):
-            for j in range(1,10):
-                exec(f"lst_in{i}.append(lst_cube.pop(0))")  
-                
-        for i in range(1,7):
-            exec(f'lst_cube.append(lst_in{i})')           
+        # lst_cube = ([x for x in parms.get("cube")])
+        #
+        # lst_in1, lst_in2, lst_in3, lst_in4, lst_in5, lst_in6 = ([] for i in range(6)) 
+        #
+        # for i in range (1,7):
+        #     for j in range(1,10):
+        #         exec(f"lst_in{i}.append(lst_cube.pop(0))")  
+        #
+        # for i in range(1,7):
+        #     exec(f'lst_cube.append(lst_in{i})')          
 
           
     except:
