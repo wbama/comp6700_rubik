00354f2b79b8f235c9a308fabdd975198d9b755b
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-23 17:28:06 -0600

    Green Light |

--- rubik/solveCheckInput.py
+++ rubik/solveCheckInput.py
@@ -27,23 +27,9 @@ def solveCheck(parms):
         unique_center = []
         for char in lst_cube_center:
             if char not in unique_center:
-                unique_center.append(char)               
+                unique_center.append(char)       
               
-        # #create an instance of the cube by creating nested list
-        # lst_cube = ([x for x in parms.get("cube")])
-        #
-        # lst_in1, lst_in2, lst_in3, lst_in4, lst_in5, lst_in6 = ([] for i in range(6)) #
-        #
-        # for i in range (1,7):
-        #     for j in range(1,10):
-        #         exec(f"lst_in{i}.append(lst_cube.pop(0))")   
-        #
-        # for i in range(1,7):
-        #     exec(f'lst_cb_out.append(lst_in{i})')          
-        #
-        #
-        # for i in range(1,7):
-        #     exec(f'lst_cube.append(lst_in{i})') 
+
           
     except:
         pass #one of the below will catch errors
