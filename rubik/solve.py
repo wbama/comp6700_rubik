8381d768cbeb586131c3195c431fe97cb8a6d512
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-23 17:20:00 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -17,9 +17,9 @@ def _solve(parms):
             for j in range(1,10):
                 exec(f"lst_in{i}.append(lst_cube.pop(0))")  
             
-        # lst_opposite_cnt = []
-        # for i in range(1,7):
-        #     exec(f'lst_cube.append(lst_in{i})')
+        lst_opposite_cnt = []
+        for i in range(1,7):
+            exec(f'lst_cube.append(lst_in{i})')
             
        
         if  parms.get("rotate") == '':
