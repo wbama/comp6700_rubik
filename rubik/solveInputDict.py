2d9521f0e236b8d54d872fc397cec6812eec1c0c
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-24 07:10:15 -0600

    added test 141

--- rubik/solveInputDict.py
+++ rubik/solveInputDict.py
@@ -1,8 +1,8 @@
 from rubik.solve import _solve
 
 inputDict = {}
-inputDict['cube'] = 'oyoobyryyyggryobbgygwygworrrwbowbgwyborgorwbgbgobrrwww'
-inputDict['rotate'] = 'f'   
+inputDict['cube'] = 'bywrbyroroyygwgbwwrbbogrrryybwwygoobowgyroorgwwyboggbg'
+#inputDict['rotate'] = 'f'   
 inputDict['op'] = 'solve' 
 
 print(_solve(inputDict))
