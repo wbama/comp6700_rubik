db4a7f60fc4e0b653b49947b5dd08b38bc777d4e
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-25 14:08:37 -0600

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -143,21 +143,21 @@ def _solve(parms):
                 flip_cube_four = flip_cube_top_side(c_rotate_cube)
                 c_rotate_cube= flip_cube_four
             
-        # else:
-        #     c_rotate_cube = (turn_clock(lst_cube))         
-       
-        str1 = "".join(c_rotate_cube[0])
-        str2 = "".join(c_rotate_cube[1])
-        str3 = "".join(c_rotate_cube[2])
-        str4 = "".join(c_rotate_cube[3])
-        str5 = "".join(c_rotate_cube[4])
-        str6 = "".join(c_rotate_cube[5])                       
-     
-        str_cube = str1+str2+str3+str4+str5+str6
-        
-        result = {}
-        result['cube'] = str_cube
-        result['status'] = 'ok'   
+            # else:
+            #     c_rotate_cube = (turn_clock(lst_cube))         
+           
+            str1 = "".join(c_rotate_cube[0])
+            str2 = "".join(c_rotate_cube[1])
+            str3 = "".join(c_rotate_cube[2])
+            str4 = "".join(c_rotate_cube[3])
+            str5 = "".join(c_rotate_cube[4])
+            str6 = "".join(c_rotate_cube[5])                       
+         
+            str_cube = str1+str2+str3+str4+str5+str6
+            
+            result = {}
+            result['cube'] = str_cube
+            result['status'] = 'ok'   
 
        
     except:
