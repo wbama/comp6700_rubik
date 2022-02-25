7defd425ff0064778afa2256f0e8c71055250cfb
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-25 14:19:36 -0600

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -27,10 +27,33 @@ def _solve(parms):
         
         if 'rotate' not in parms:
                 c_rotate_cube = (turn_clock(lst_cube)) 
-                result['cube'] = c_rotate_cube
+                str1 = "".join(c_rotate_cube[0])
+                str2 = "".join(c_rotate_cube[1])
+                str3 = "".join(c_rotate_cube[2])
+                str4 = "".join(c_rotate_cube[3])
+                str5 = "".join(c_rotate_cube[4])
+                str6 = "".join(c_rotate_cube[5])                       
+         
+                str_cube = str1+str2+str3+str4+str5+str6                    
+                result = {}
+                result['cube'] = str_cube
                 result['status'] = 'ok' 
+                
+        if 'rotate' in parms and len(parms.get('rotate')) == 0:
+                c_rotate_cube = (turn_clock(lst_cube)) 
+                str1 = "".join(c_rotate_cube[0])
+                str2 = "".join(c_rotate_cube[1])
+                str3 = "".join(c_rotate_cube[2])
+                str4 = "".join(c_rotate_cube[3])
+                str5 = "".join(c_rotate_cube[4])
+                str6 = "".join(c_rotate_cube[5])                       
+         
+                str_cube = str1+str2+str3+str4+str5+str6                    
+                result = {}
+                result['cube'] = str_cube
+                result['status'] = 'ok'   
         
-        if 'rotate' in parms and len(parms.get("rotate")) >= 0:
+        if 'rotate' in parms:
             for rotation in (parms.get('rotate')):   
                 print(rotation)         
             
@@ -147,18 +170,18 @@ def _solve(parms):
                 else:
                     c_rotate_cube = (turn_clock(lst_cube))         
            
-                str1 = "".join(c_rotate_cube[0])
-                str2 = "".join(c_rotate_cube[1])
-                str3 = "".join(c_rotate_cube[2])
-                str4 = "".join(c_rotate_cube[3])
-                str5 = "".join(c_rotate_cube[4])
-                str6 = "".join(c_rotate_cube[5])                       
+            str1 = "".join(c_rotate_cube[0])
+            str2 = "".join(c_rotate_cube[1])
+            str3 = "".join(c_rotate_cube[2])
+            str4 = "".join(c_rotate_cube[3])
+            str5 = "".join(c_rotate_cube[4])
+            str6 = "".join(c_rotate_cube[5])                       
          
-                str_cube = str1+str2+str3+str4+str5+str6
+            str_cube = str1+str2+str3+str4+str5+str6
                     
-                result = {}
-                result['cube'] = str_cube
-                result['status'] = 'ok'   
+            result = {}
+            result['cube'] = str_cube
+            result['status'] = 'ok'   
 
        
     except:
