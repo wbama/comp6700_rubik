b91ec0400a50c53ee922fde9e3f33fdc6e788c24
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-25 17:21:34 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -24,17 +24,18 @@ def _solve(parms):
        
         if 'rotate' not in parms:
                 c_rotate_cube = (turn_clock(lst_cube)) 
-                str1 = "".join(c_rotate_cube[0])
-                str2 = "".join(c_rotate_cube[1])
-                str3 = "".join(c_rotate_cube[2])
-                str4 = "".join(c_rotate_cube[3])
-                str5 = "".join(c_rotate_cube[4])
-                str6 = "".join(c_rotate_cube[5])                       
-         
-                str_cube = str1+str2+str3+str4+str5+str6                    
-                result = {}
-                result['cube'] = str_cube
-                result['status'] = 'ok' 
+                # str1 = "".join(c_rotate_cube[0])
+                # str2 = "".join(c_rotate_cube[1])
+                # str3 = "".join(c_rotate_cube[2])
+                # str4 = "".join(c_rotate_cube[3])
+                # str5 = "".join(c_rotate_cube[4])
+                # str6 = "".join(c_rotate_cube[5])                       
+                #
+                # str_cube = str1+str2+str3+str4+str5+str6                    
+                # result = {}
+                # result['cube'] = str_cube
+                # result['status'] = 'ok' 
+                stringjoins(c_rotate_cube)
                 
         if 'rotate' in parms and (parms.get('rotate')) == None:
                 c_rotate_cube = (turn_clock(lst_cube)) 
