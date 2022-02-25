5aa19abe39dd3c4fe1db3386539826f71e45f345
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-25 15:48:33 -0600

    Red Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -177,7 +177,7 @@ class SolveTest(unittest.TestCase):
 
         inputDict = {}
         inputDict['cube'] = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        inputDict['rotate'] = 
+        inputDict['rotate'] = ''
         inputDict['op'] = 'solve'           
 
         expectedResult = {}
@@ -186,7 +186,22 @@ class SolveTest(unittest.TestCase):
         
         actualResult = solve._solve(inputDict) #calling _solve and passing inputDict
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
-        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))     
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))    
+        
+    def test_131_ShouldRotateValidCube_RotateNone(self):
+
+        inputDict = {}
+        inputDict['cube'] = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
+        inputDict['rotate'] = None
+        inputDict['op'] = 'solve'           
+
+        expectedResult = {}
+        expectedResult['cube'] = 'gggggggggwrrwrrwrrbbbbbbbbbooyooyooywwwwwwooorrryyyyyy'
+        expectedResult['status'] = 'ok'
+        
+        actualResult = solve._solve(inputDict) #calling _solve and passing inputDict
+        self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
+        self.assertEqual(expectedResult.get('status'), actualResult.get('status'))   
 
         
     def test_140_ShouldRotateValidCube_RotateMissing(self):
