14e306857b6dcc8a5f7c8e06ba926710ba435a3b
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-25 14:29:50 -0600

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -168,12 +168,12 @@ def _solve(parms):
                 # else:
                 #     c_rotate_cube = (turn_clock(lst_cube))         
            
-            str1 = "".join(c_rotate_cube[0])
-            str2 = "".join(c_rotate_cube[1])
-            str3 = "".join(c_rotate_cube[2])
-            str4 = "".join(c_rotate_cube[3])
-            str5 = "".join(c_rotate_cube[4])
-            str6 = "".join(c_rotate_cube[5])                       
+            str1 = "".join(lst_cube[0])
+            str2 = "".join(lst_cube[1])
+            str3 = "".join(lst_cube[2])
+            str4 = "".join(lst_cube[3])
+            str5 = "".join(lst_cube[4])
+            str6 = "".join(lst_cube[5])                       
          
             str_cube = str1+str2+str3+str4+str5+str6
                     
