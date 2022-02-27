883be355b1483c2f5fe811e285f1ee3eba4f5572
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-27 09:22:50 -0600

    Green Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -34,7 +34,7 @@ def _solve(parms):
                 
                 str_cube = str1+str2+str3+str4+str5+str6        
                 result = {}
-                result['cube'] = stringjoins(lst_cube)
+                result['cube'] = str_cube
                 result['status'] = 'ok' 
                 
         if 'rotate' in parms and (parms.get('rotate')) == None:
