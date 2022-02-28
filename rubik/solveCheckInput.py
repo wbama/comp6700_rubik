3ba5a2bc575f7c3cb8a99f2ad04ef575fd5aec02
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-02-28 15:03:46 -0600

    Red Light |

--- rubik/solveCheckInput.py
+++ rubik/solveCheckInput.py
@@ -80,7 +80,7 @@ def solveCheck(parms):
                 result['status'] = ('error: optional rotate should be in [FfRrBbLlUuDd], None or ""')               
                   
             
-    elif (parms.get('cube', None)).isalpha() != True:
+    elif (parms.get('cube', None)).isalpha() == False:
         result['status'] = ('error: only letters and numbers in the cube string')
        
     else:
