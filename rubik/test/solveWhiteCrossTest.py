06e9c46ebb17b2cf409d1b20f4a543f4c6c1ec7e
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-03 12:18:54 -0600

    Red Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -23,11 +23,10 @@ class SolveWhiteCrossTest(unittest.TestCase):
             inputDict['cube'] = 'ggrggoggoyroyyygbrbbbgbbybbwwwwwwwwwrryrryrrbooyooyoog'
             inputDict['op'] = 'solve'  
             
-            actualResult = createCubeListFromInputParms(inputDict)         
-    
+            cubeList = createCubeListFromInputParms(inputDict)     
             expectedResult = [['g', 'g', 'g', 'g', 'g', 'g', 'o', 'o', 'r'], ['r', 'r', 'r', 'r', 'r', 'r', 'b', 'y', 'y'], ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'g', 'y'], ['o', 'o', 'o', 'o', 'o', 'o', 'g', 'y', 'y'], ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'], ['g', 'y', 'y', 'b', 'y', 'r', 'r', 'y', 'o']]
 
 
-            actualResult = rotateCubeClockwise(inputDict)
+            actualResult = rotateCubeClockwise(cubeList)
             self.assertEqual(expectedResult, actualResult)
     
