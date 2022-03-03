0316d2fb1b0cfdea1f1607d83a544e187ba182c9
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-03 12:33:15 -0600

    Red Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -41,7 +41,8 @@ class SolveWhiteCrossTest(unittest.TestCase):
             print(str5)
             str6 = "".join(rotatedCube[5]) 
             print(str6)
-            str_cube = str1+str2     
+            str_cube = str1+str2    
+            print(str_cube) 
 
               
             self.assertEqual(expectedResult.get('cube'), str_cube)
