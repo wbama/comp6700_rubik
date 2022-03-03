d4c042ff0c51cf2e3a5f5fa7eddfee9ca268df45
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-03 12:15:45 -0600

    Red Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -1,6 +1,7 @@
 import unittest
 import rubik.solveWhiteCross as sc
 from rubik.solveRotations import rotateCubeClockwise
+from rubik.solveRotations import createCubeListFromInputParms
 
 class SolveWhiteCrossTest(unittest.TestCase):
     
@@ -20,7 +21,9 @@ class SolveWhiteCrossTest(unittest.TestCase):
         def test_020_RotateWholeCubeClockwise(self):
             inputDict = {}
             inputDict['cube'] = 'ggrggoggoyroyyygbrbbbgbbybbwwwwwwwwwrryrryrrbooyooyoog'
-            inputDict['op'] = 'solve'           
+            inputDict['op'] = 'solve'  
+            
+            inputResult = createCubeListFromInputParms(inputDict)         
     
             expectedResult = [['g', 'g', 'g', 'g', 'g', 'g', 'o', 'o', 'r'], ['r', 'r', 'r', 'r', 'r', 'r', 'b', 'y', 'y'], ['b', 'b', 'b', 'b', 'b', 'b', 'b', 'g', 'y'], ['o', 'o', 'o', 'o', 'o', 'o', 'g', 'y', 'y'], ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'], ['g', 'y', 'y', 'b', 'y', 'r', 'r', 'y', 'o']]
 
