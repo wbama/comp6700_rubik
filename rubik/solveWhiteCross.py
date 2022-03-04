c23898ac74a699a99299ff614c1ce0ed9979a0e1
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-04 14:46:18 -0600

    Green Light |

--- rubik/solveWhiteCross.py
+++ rubik/solveWhiteCross.py
@@ -3,8 +3,8 @@ from rubik.solveRotations import rotateCubeToLeft
 from rubik.solveRotations import rotateCubeToRight
 from rubik.solveRotations import rotateCubeToTop
 from rubik.solveRotations import createStringFromCube
-from rubik.solveRotations import turnSideCounterClock
-from rubik.solveRotations import turnSideClock
+from rubik.solveRotations import rotateSideCounterClock
+from rubik.solveRotations import rotateSideClock
 
 
 
@@ -48,40 +48,40 @@ def solveWhiteCross(parms):
     if lst_cube[0][5] != 'w':
         if lst_cube[4][5] == 'w':
             lst_cube = rotateCubeToLeft(lst_cube)
-            cc_rotate_cube = turnSideCounterClock(lst_cube)
+            cc_rotate_cube = rotateSideCounterClock(lst_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
         elif lst_cube[2][1] == 'w': 
             lst_cube = rotateCubeToLeft(lst_cube)
-            cc_rotate_cube = turnSideCounterClock(lst_cube)
-            cc_rotate_cube = turnSideCounterClock(cc_rotate_cube)
+            cc_rotate_cube = rotateSideCounterClock(lst_cube)
+            cc_rotate_cube = rotateSideCounterClock(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube) 
         elif lst_cube[5][5] == 'w': 
             lst_cube = rotateCubeToLeft(lst_cube)
-            cc_rotate_cube = turnSideClock(lst_cube)
+            cc_rotate_cube = rotateSideClock(lst_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube) 
         elif lst_cube[1][3] == 'w': 
             lst_cube = rotateCubeToLeft(lst_cube)
-            c_rotate_cube = turnSideClock(lst_cube)
+            c_rotate_cube = rotateSideClock(lst_cube)
             lst_cube = rotateCubeToLeft(c_rotate_cube)
             lst_cube = rotateCubeToLeft(lst_cube)
             lst_cube = rotateCubeToLeft(lst_cube) 
             lst_cube = rotateCubeToTop(lst_cube) 
-            c_rotate_cube = turnSideClock(lst_cube)
+            c_rotate_cube = rotateSideClock(lst_cube)
             lst_cube = rotateCubeToTop(c_rotate_cube)
             lst_cube = rotateCubeToTop(c_rotate_cube)
             lst_cube = rotateCubeToTop(c_rotate_cube)
         elif lst_cube[2][1] == 'w': 
             lst_cube = rotateCubeToLeft(lst_cube)
-            c_rotate_cube = turnSideClock(lst_cube)
+            c_rotate_cube = rotateSideClock(lst_cube)
             lst_cube = rotateCubeToLeft(c_rotate_cube)
             lst_cube = rotateCubeToLeft(lst_cube)
             lst_cube = rotateCubeToLeft(lst_cube) 
             lst_cube = rotateCubeToTop(lst_cube) 
-            c_rotate_cube = turnSideClock(lst_cube)
+            c_rotate_cube = rotateSideClock(lst_cube)
             lst_cube = rotateCubeToTop(c_rotate_cube)
             lst_cube = rotateCubeToTop(c_rotate_cube)
             lst_cube = rotateCubeToTop(c_rotate_cube)
