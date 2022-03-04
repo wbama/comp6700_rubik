e463b4faa77e3b7027a2432d4ba8e38a62b0f54e
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-04 14:42:06 -0600

    Green Light |

--- rubik/solveWhiteCross.py
+++ rubik/solveWhiteCross.py
@@ -3,8 +3,8 @@ from rubik.solveRotations import rotateCubeToLeft
 from rubik.solveRotations import rotateCubeToRight
 from rubik.solveRotations import rotateCubeToTop
 from rubik.solveRotations import createStringFromCube
-from rubik.solveRotations import turn_cclock
-from rubik.solveRotations import turn_clock
+from rubik.solveRotations import turnSideCounterClock
+
 
 
 def solveWhiteCross(parms):
@@ -47,12 +47,12 @@ def solveWhiteCross(parms):
     if lst_cube[0][5] != 'w':
         if lst_cube[4][5] == 'w':
             lst_cube = rotateCubeToLeft(lst_cube)
-            cc_rotate_cube = turn_cclock(lst_cube)
+            cc_rotate_cube = turnSideCounterClock(lst_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
         elif lst_cube[2][1] == 'w': 
             lst_cube = rotateCubeToLeft(lst_cube)
-            cc_rotate_cube = turn_cclock(lst_cube)
-            cc_rotate_cube = turn_cclock(cc_rotate_cube)
+            cc_rotate_cube = turnSideCounterClock(lst_cube)
+            cc_rotate_cube = turnSideCounterClock(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube)
             lst_cube = rotateCubeToLeft(cc_rotate_cube) 
