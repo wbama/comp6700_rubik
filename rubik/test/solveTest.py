7b8a682a6aa78bba38215674a87df19b8bea7ed8
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-04 12:19:33 -0600

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -273,7 +273,7 @@ class SolveTest(unittest.TestCase):
     def test_940_InvalidRotation(self):
         inputDict = {}
         inputDict['cube'] = 'gggggggggrrrrrrrrrbbbbbbbbbooooooooowwwwwwwwwyyyyyyyyy'
-        inputDict['rotate'] = 'w'
+        inputDict['rotate'] = 'Ff Bb'
         inputDict['op'] = 'solve'
 
         expectedResult = {}
