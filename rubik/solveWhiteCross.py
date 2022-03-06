8c501590cb143a9bdd59510246e795f65b18de05
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-06 16:37:07 -0600

    Red Light |

--- rubik/solveWhiteCross.py
+++ rubik/solveWhiteCross.py
@@ -84,14 +84,30 @@ def solveWhiteCross(parms):
         
     # have the daisy now. match up colors and rotate sides down for white cross
     
-    while lst_cube[0][5] != lst_cube[1][3]:
-        lst_cube = rotateSideClock(lst_cube)  
-
-    else:
+    if lst_cube[0][5] == lst_cube[1][3]:
         lst_cube = rotateCubeToRight(lst_cube)
         lst_cube = rotateSideClock(lst_cube)
         lst_cube = rotateSideClock(lst_cube)
-        lst_cube = rotateCubeToLeft(lst_cube)    
+        lst_cube = rotateCubeToLeft(lst_cube)  
+    elif lst_cube[0][5] == lst_cube[4][7]:
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateCubeToRight(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateCubeToLeft(lst_cube)  
+    elif lst_cube[0][5] == lst_cube[3][5]:
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateCubeToRight(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateCubeToLeft(lst_cube)  
+    elif lst_cube[0][5] == lst_cube[5][1]:
+        lst_cube = rotateSideCounterClock(lst_cube)
+        lst_cube = rotateCubeToRight(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateSideClock(lst_cube)
+        lst_cube = rotateCubeToLeft(lst_cube)  
         
        
     #make white cross the front
