60867ad991dc7d0561560a62dea73c967d884196
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-06 17:24:44 -0600

    Red Light |

--- rubik/solveWhiteCross.py
+++ rubik/solveWhiteCross.py
@@ -85,10 +85,10 @@ def solveWhiteCross(parms):
     # have the daisy now. match up colors and rotate sides down for white cross
     
     if lst_cube[1][3] == lst_cube[1][4]:
-        lst_cube = rotateCubeToRight(lst_cube)
+        lst_cube = rotateCubeToLeft(lst_cube)
         lst_cube = rotateSideClock(lst_cube)
         lst_cube = rotateSideClock(lst_cube)
-        lst_cube = rotateCubeToLeft(lst_cube)  
+        lst_cube = rotateCubeToRight(lst_cube)  
     elif lst_cube[1][3] == lst_cube[4][4]:
         lst_cube = rotateSideCounterClock(lst_cube)
         lst_cube = rotateCubeDown(lst_cube)
