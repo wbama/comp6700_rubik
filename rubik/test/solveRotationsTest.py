e3c7170e61306209337d581726a05d1c6b245a38
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-07 09:20:40 -0600

    Red Light |

--- rubik/test/solveRotationsTest.py
+++ rubik/test/solveRotationsTest.py
@@ -185,6 +185,28 @@ class SolveDaisyTest(unittest.TestCase):
                         
             self.assertEqual(expectedResult.get('cube'), str_cube) 
             
+        def test_050_TestRotateCube_b(self):
+            inputDict = {}
+            inputDict['cube'] = 'gwrwybbgggwyorboybbrgrwyowwrgyoogoywwyrogboryrrwbbgboy'
+ 
+    
+            expectedResult = {}
+            expectedResult['cube'] = 'gwrwybbgggwworyoyrgywrwwbrobgyoogyywoorogboryrrwbbgbby'
+            
+            lst_cube = createCubeListFromInputParms(inputDict)  
+            rotatedCube = rotateSide_b(lst_cube)
+            
+            str1 = "".join(rotatedCube[0])
+            str2 = "".join(rotatedCube[1])
+            str3 = "".join(rotatedCube[2])
+            str4 = "".join(rotatedCube[3])
+            str5 = "".join(rotatedCube[4])
+            str6 = "".join(rotatedCube[5]) 
+
+            str_cube = str1+str2+str3+str4+str5+str6  
+                        
+            self.assertEqual(expectedResult.get('cube'), str_cube) 
+            
  
         
 
