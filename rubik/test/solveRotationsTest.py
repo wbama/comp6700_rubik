f797a3a2e9315414157c44fa348485fdf7df80e6
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-07 09:57:42 -0600

    Red Light |

--- rubik/test/solveRotationsTest.py
+++ rubik/test/solveRotationsTest.py
@@ -5,6 +5,9 @@ from rubik.solveRotations import rotateCubeUp, rotateCubeDown
 from rubik.solveRotations import rotateCubeClock
 from rubik.solveRotations import rotateSide_R, rotateSide_r
 from rubik.solveRotations import rotateSide_B, rotateSide_b
+from rubik.solveRotations import rotateSide_L, rotateSide_l
+from rubik.solveRotations import rotateSide_U, rotateSide_u
+from rubik.solveRotations import rotateSide_D, rotateSide_d
 
 class SolveDaisyTest(unittest.TestCase):
             
@@ -195,6 +198,28 @@ class SolveDaisyTest(unittest.TestCase):
             
             lst_cube = createCubeListFromInputParms(inputDict)  
             rotatedCube = rotateSide_b(lst_cube)
+            
+            str1 = "".join(rotatedCube[0])
+            str2 = "".join(rotatedCube[1])
+            str3 = "".join(rotatedCube[2])
+            str4 = "".join(rotatedCube[3])
+            str5 = "".join(rotatedCube[4])
+            str6 = "".join(rotatedCube[5]) 
+
+            str_cube = str1+str2+str3+str4+str5+str6  
+                        
+            self.assertEqual(expectedResult.get('cube'), str_cube) 
+            
+        def test_050_TestRotateCube_L(self):
+            inputDict = {}
+            inputDict['cube'] = 'rbrwwgbwbwywoggrbborbyygwyyrbyrbooryybgwrrgogogwwoygoo'
+ 
+    
+            expectedResult = {}
+            expectedResult['cube'] = 'ybrwwggwbwywoggrbborgyywwyoorrrbbyoyybggrrbogrgwwoyboo'
+            
+            lst_cube = createCubeListFromInputParms(inputDict)  
+            rotatedCube = rotateSide_L(lst_cube)
             
             str1 = "".join(rotatedCube[0])
             str2 = "".join(rotatedCube[1])
