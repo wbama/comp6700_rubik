d1006eaa75b869532873243180bb7f7a4146b6dc
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-06 18:11:49 -0600

    Green Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -11,11 +11,11 @@ class SolveWhiteCrossTest(unittest.TestCase):
             expectedResult = 'w'             
 
             actualResult = solveWhiteCross(inputDict)
-            self.assertEqual(expectedResult, actualResult.get('cube')[1])
-            self.assertEqual(expectedResult, actualResult.get('cube')[3])
-            self.assertEqual(expectedResult, actualResult.get('cube')[4])
-            self.assertEqual(expectedResult, actualResult.get('cube')[5])
-            self.assertEqual(expectedResult, actualResult.get('cube')[7])
+            self.assertEqual(expectedResult, actualResult[0][1])
+            self.assertEqual(expectedResult, actualResult[0][3])
+            self.assertEqual(expectedResult, actualResult[0][4])
+            self.assertEqual(expectedResult, actualResult[0][5])
+            self.assertEqual(expectedResult, actualResult[0][7])
             
         
 
