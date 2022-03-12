af2044a9f3d07d791c8b841ead61f76e283b2ab0
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-12 14:58:53 -0600

    Red Light |

--- rubik/solveDaisy.py
+++ rubik/solveDaisy.py
@@ -25,69 +25,14 @@ def solveDaisy(parms):
               
     #first leaf position [0][5]
     #solve only this position. Then rotate the front edge and solve it again and again
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
+        for i in range(5):
+            while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
+                lst_cube = rotateSide_F(lst_cube)
+                lst_rotate.append("F")
+            else:
+                lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
             
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
+ 
             
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-            
-    #yellow to the right        
-    if createCubeListFromInputParms(parms)[1][4] == 'y':
-              
-    #first leaf position [0][5]
-    #solve only this position. Then rotate the front edge and solve it again and again
-        while (lst_cube[1][5] != "w" or  lst_cube[1][1] != "w" or lst_cube[1][3] != "w" or lst_cube[1][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
-            
-        while (lst_cube[0][5] != "w" or  lst_cube[0][1] != "w" or lst_cube[0][3] != "w" or lst_cube[0][7] != "w") and lst_cube[0][5] == "w":
-            lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F")
-        else:
-            lst_cube = (y_0_WhiteLeafPos0_5(lst_cube, lst_rotate))[0] 
+
     return lst_cube, lst_rotate
