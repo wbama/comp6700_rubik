75c397c6611df5926f661280bff35237572ade7f
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-13 08:58:32 -0500

    Red Light |

--- rubik/solveDaisy.py
+++ rubik/solveDaisy.py
@@ -35,8 +35,8 @@ def solveDaisy(parms):
         
         for i in range(5):
             while (lst_cube[1][1] != "w" or lst_cube[1][3] != "w" or lst_cube[1][7] != "w") and lst_cube[0][5] == "w":
-                lst_cube = rotateSide_F(lst_cube)
-                lst_rotate.append("F")
+                lst_cube = rotateSide_R(lst_cube)
+                lst_rotate.append("R")
             else:
                 lst_cube = (y_1_WhiteLeafPos1_5(lst_cube, lst_rotate))[0]  
                 
