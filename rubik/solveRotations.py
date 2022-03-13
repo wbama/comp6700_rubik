a9b3d9fb559781cb289f1e2329a0d78f3308e69e
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-13 11:16:15 -0500

    Red Light |

--- rubik/solveRotations.py
+++ rubik/solveRotations.py
@@ -1089,7 +1089,7 @@ def rotateCubeToLeft(cube):
  
 def y_0_WhiteLeafPos0_5(lst_cube, lst_rotate):
     
-    #easiest movements first
+    #one line of movement - first and easiest
         if lst_cube[4][5] == 'w' and lst_cube[0][5] != "w":
             lst_cube = rotateSide_r(lst_cube)
             lst_rotate.append("r") 
@@ -1129,9 +1129,9 @@ def y_0_WhiteLeafPos0_5(lst_cube, lst_rotate):
             lst_rotate.append("dd") 
         elif lst_cube[3][7] == 'w' and lst_cube[0][7] != "w":
             lst_cube = rotateSide_D(lst_cube)
-            lst_rotate.append("D")            
-        #rotate to non white in [0][5]
-        
+            lst_rotate.append("D")
+                        
+        #rotate to non white in [0][5]        
         elif lst_cube[0][5] == "w" and lst_cube[0][7] != "w":
             lst_cube = rotateSide_f(lst_cube)
             lst_rotate.append("f")  
@@ -1141,20 +1141,9 @@ def y_0_WhiteLeafPos0_5(lst_cube, lst_rotate):
             lst_rotate.append("FF")  
         elif lst_cube[0][1] == "w" and lst_cube[0][7] != "w":
             lst_cube = rotateSide_F(lst_cube)
-            lst_rotate.append("F") 
-        
-        # #solve [0][5]
-        # if lst_cube[0][5] == "w":
-        #     lst_cube = lst_cube
-        #     lst_rotate.append("") 
-        
-        elif lst_cube[5][5] == 'w': 
-            lst_cube = rotateSide_R(lst_cube)
-            lst_rotate.append("R")   
-        elif lst_cube[4][5] == 'w':
-            lst_cube = rotateSide_r(lst_cube)
-            lst_rotate.append("r") 
-        #two lines of movement
+            lst_rotate.append("F")  
+
+        #now solve for [0][5] in each case
         elif lst_cube[2][3] == 'w':
             lst_cube = rotateSide_R(lst_cube)
             lst_cube = rotateSide_R(lst_cube)
