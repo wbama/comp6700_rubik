3bdc2010636c8718b9b93ff750a9ce9d71812cdd
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-13 10:26:45 -0500

    Red Light |

--- rubik/solveRotations.py
+++ rubik/solveRotations.py
@@ -1239,16 +1239,60 @@ def y_0_WhiteLeafPos0_5(lst_cube, lst_rotate):
         return lst_cube, lst_rotate
     
 def y_1_WhiteLeafPos1_5(lst_cube, lst_rotate):
-
-        if lst_cube[1][5] == "w":
-            lst_cube = lst_cube
-            lst_rotate.append("")         
+    
+    #easiest movements first
+    #put white in 1-5
+        if lst_cube[4][1] == 'w':
+            lst_cube = rotateSide_b(lst_cube)
+            lst_rotate.append("b") 
+        elif lst_cube[3][3] == 'w':
+            lst_cube = rotateSide_b(lst_cube)
+            lst_cube = rotateSide_b(lst_cube)            
+        elif lst_cube[5][7] == 'w':
+            lst_rotate.append("B")  
+            lst_rotate.append("bb")             
+    #put white in 1-1
+        elif lst_cube[0][1] == 'w':
+            lst_cube = rotateSide_u(lst_cube)
+            lst_rotate.append("u") 
+        elif lst_cube[3][1] == 'w':
+            lst_cube = rotateSide_u(lst_cube)
+            lst_cube = rotateSide_u(lst_cube)
+            lst_rotate.append("uu") 
+        elif lst_cube[2][1] == 'w':
+            lst_cube = rotateSide_U(lst_cube)
+            lst_rotate.append("U")     
+    #put white in 1-3 
+        elif lst_cube[4][7] == 'w':
+            lst_cube = rotateSide_F(lst_cube)
+            lst_rotate.append("F") 
+        elif lst_cube[5][1] == 'w':
+            lst_cube = rotateSide_f(lst_cube)
+            lst_rotate.append("f") 
+        elif lst_cube[3][5] == 'w':
+            lst_cube = rotateSide_F(lst_cube)
+            lst_cube = rotateSide_F(lst_cube)
+            lst_rotate.append("FF") 
+    #put white in 1-7
+        elif lst_cube[0][7] == 'w':
+            lst_cube = rotateSide_D(lst_cube)
+            lst_rotate.append("D") 
+        elif lst_cube[2][7] == 'w':
+            lst_cube = rotateSide_d(lst_cube)
+            lst_rotate.append("d") 
+        elif lst_cube[3][7] == 'w':
+            lst_cube = rotateSide_D(lst_cube)
+            lst_cube = rotateSide_D(lst_cube)
+            lst_rotate.append("DD")            
+        #now run again to put in [1][5]
+    
+        # elif lst_cube[1][5] == "w":
+        #     lst_cube = lst_cube
+        #     lst_rotate.append("")         
         elif lst_cube[5][7] == 'w': 
             lst_cube = rotateSide_B(lst_cube)
             lst_rotate.append("B")   
-        elif lst_cube[4][1] == 'w':
-            lst_cube = rotateSide_b(lst_cube)
-            lst_rotate.append("b") 
+
         #two lines of movement
         elif lst_cube[3][3] == 'w':
             lst_cube = rotateSide_B(lst_cube)
