6fef16d0577a757e59008f93fcbb76472c56a841
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-14 15:47:38 -0500

    Red Light |

--- rubik/solveRotations.py
+++ rubik/solveRotations.py
@@ -2127,7 +2127,7 @@ def rotateIntoWhiteCross_y_5(lst_cube, lst_rotate):
 
 def createYellowAndWhiteSides(parms):
     lst_cube = createCubeListFromInputParms(parms)
-    if ("y") in (parms.get("cube").lower()):
+    if ("y") in (parms.get("cube")) or "Y" in (parms.get("cube")):
         if ("y" in lst_cube[0][4] or "Y" in lst_cube[0][4]):
             var_w = lst_cube[2][4]
             var_y = lst_cube[0][4]
