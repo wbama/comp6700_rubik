8fac8a2199eb1a56e2ee61894b9be22e92aa25ef
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-14 15:59:41 -0500

    Red Light |

--- rubik/solveDaisy.py
+++ rubik/solveDaisy.py
@@ -19,38 +19,36 @@ def solveDaisy(parms):
     #find out where the yellow center is. Solve from that vantage point    
     lst_cube = (createCubeListFromInputParms(parms)) 
     var_y = createYellowAndWhiteSides(parms)[0]
-    print(var_y)
     var_w = createYellowAndWhiteSides(parms)[1]
-    print(var_w)
     lst_rotate = [] 
 
     #yellow is in front    
-    if createCubeListFromInputParms(parms)[0][4] == 'y': 
+    if createCubeListFromInputParms(parms)[0][4] == var_y: 
         #just run through the code 20 times. Take out the no movements in the end
         for i in range(20):
             lst_cube = (y_0_SolveWhiteLeaves(lst_cube, lst_rotate))[0]         
  
-    if createCubeListFromInputParms(parms)[1][4] == 'y': 
+    if createCubeListFromInputParms(parms)[1][4] == var_y: 
         #just run through the code 20 times. Take out the no movements in the end
         for i in range(20):
             lst_cube = (y_1_SolveWhiteLeaves(lst_cube, lst_rotate))[0] 
 
-    if createCubeListFromInputParms(parms)[2][4] == 'y': 
+    if createCubeListFromInputParms(parms)[2][4] == var_y: 
         #just run through the code 20 times. Take out the no movements in the end
         for i in range(20):
             lst_cube = (y_2_SolveWhiteLeaves(lst_cube, lst_rotate))[0]    
             
-    if createCubeListFromInputParms(parms)[3][4] == 'y': 
+    if createCubeListFromInputParms(parms)[3][4] == var_y: 
         #just run through the code 20 times. Take out the no movements in the end
         for i in range(20):
             lst_cube = (y_3_SolveWhiteLeaves(lst_cube, lst_rotate))[0]    
             
-    if createCubeListFromInputParms(parms)[4][4] == 'y': 
+    if createCubeListFromInputParms(parms)[4][4] == var_y: 
         #just run through the code 20 times. Take out the no movements in the end
         for i in range(20):
             lst_cube = (y_4_SolveWhiteLeaves(lst_cube, lst_rotate))[0] 
             
-    if createCubeListFromInputParms(parms)[5][4] == 'y': 
+    if createCubeListFromInputParms(parms)[5][4] == var_y: 
         #just run through the code 20 times. Take out the no movements in the end
         for i in range(20):
             lst_cube = (y_5_SolveWhiteLeaves(lst_cube, lst_rotate))[0]              
