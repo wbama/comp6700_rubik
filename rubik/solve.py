5af3257e7b4bd4d0d553451fa0eead4b80a14fa7
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-14 15:20:27 -0500

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -11,27 +11,6 @@ def _solve(parms):
     result = {}    
     str_cube = ''
     
-    lst_cube = createCubeListFromInputParms(parms)
-    if ("y") in lst_cube:
-        if ("y" in lst_cube[0][4] or "Y" in lst_cube[0][4]):
-            var_w = lst_cube[2][4]
-            var_y = lst_cube[0][4]
-        elif ("y"  in lst_cube[1][4] or "Y" in lst_cube[1][4]):
-            var_w = lst_cube[3][4]
-            var_y = lst_cube[1][4]
-        elif ("y" in lst_cube[2][4] or "Y" in lst_cube[2][4])  :
-            var_w = lst_cube[0][4] 
-            var_y = lst_cube[2][4]       
-        elif ("y" in lst_cube[3][4] or "Y" in lst_cube[3][4])  :
-            var_w = lst_cube[1][4]
-            var_y = lst_cube[3][4]
-        elif ("y" in lst_cube[4][4] or "Y" in lst_cube[4][4])  :
-            var_w = lst_cube[5][4]
-            var_y = lst_cube[4][4]
-    else:
-        var_y = lst_cube[0][4]
-        var_w = lst_cube[2][4]
-    
     try:         
 
         
