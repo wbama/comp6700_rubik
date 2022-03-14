4cf2b4dfffceab0cd33ef917ebd4e9d8dbcf8778
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-14 13:09:09 -0500

    Red Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -54,7 +54,7 @@ class SolveWhiteCrossTest(unittest.TestCase):
             
         def test_050_SolveWhiteCross_y4(self):
             inputDict = {}
-            inputDict['cube'] = 'ybbbbwggboywrrbygwrgoygyroggobrorryowwbwygowwyrrowoybg'    
+            inputDict['cube'] = 'orgyowbogwgwbbyrgbgoborwyygyrbrgbyywowrbywwgorbygwoorr'    
             expectedResult = 'w'             
 
             actualResult = solveWhiteCross(inputDict)[0]
