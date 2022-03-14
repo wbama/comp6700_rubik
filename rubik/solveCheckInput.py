f9b31345ee8025cd87f55ad2e7df9f8f1412d4aa
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-14 14:07:08 -0500

    Red Light |

--- rubik/solveCheckInput.py
+++ rubik/solveCheckInput.py
@@ -79,6 +79,9 @@ def solveCheck(parms):
         result['status'] = ('error: one of the sides has to be white')  
         
     #only certain rotations are accepted or blank 
+    elif 'rotate' in parms (parms.get('rotate')) == None:
+            result['status'] = ('error: optional rotate should be in [FfRrBbLlUuDd]')  
+            
     elif 'rotate' in parms and " " in (parms.get('rotate')):
             result['status'] = ('error: optional rotate should be in [FfRrBbLlUuDd]')  
                
