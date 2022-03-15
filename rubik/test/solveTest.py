344ffe39542ed3e9ca73d1712ae4af4461984ae6
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-15 10:10:41 -0500

    Red Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -196,7 +196,7 @@ class SolveTest(unittest.TestCase):
         inputDict['op'] = 'solve'           
 
         expectedResult = {}
-        expectedResult['cube'] = 'bbbbbbbbbrrrrrrrrrgggggggggoooooooooyyyyyyyyywwwwwwwww'
+        expectedResult['cube'] = None
         expectedResult['status'] = 'ok'
         actualResult = solve._solve(inputDict)
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
