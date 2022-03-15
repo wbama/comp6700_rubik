7730e73abb19aa2d9106224492535a8cdb0bdadd
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-15 10:12:15 -0500

    Red Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -188,7 +188,7 @@ class SolveTest(unittest.TestCase):
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
         self.assertEqual(expectedResult.get('status'), actualResult.get('status')) 
         
-    def test_130_ShouldGiveValidNominalCubeNoRotate(self):
+    def test_130_ShouldGiveValidCubeMissingRotate(self):
 
         inputDict = {}
         inputDict['cube'] = 'ybbbbwggboywrrbygwrgoygyroggobrorryowwbwygowwyrrowoybg'
@@ -197,7 +197,7 @@ class SolveTest(unittest.TestCase):
 
         expectedResult = {}
         expectedResult['cube'] = None
-        expectedResult['solution'] = ""
+        expectedResult['solution'] = "RRRBBLLFF"
         expectedResult['status'] = 'ok'
         actualResult = solve._solve(inputDict)
         self.assertEqual(expectedResult.get('cube'), actualResult.get('cube'))
