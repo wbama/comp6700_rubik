713bf5ce355d70169358f493334e975a4f42b7e1
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-15 10:19:31 -0500

    Red Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -88,6 +88,18 @@ class SolveWhiteCrossTest(unittest.TestCase):
             self.assertEqual(expectedResult, actualResult[4][5])
             self.assertEqual(expectedResult, actualResult[4][7])
             
+        def test_010_SolveWhiteCross_y0_NoYellow(self):
+            inputDict = {}
+            inputDict['cube'] = 'rwrr1googg1r1b1ow1bbbbwrro1oogwggbr1wowgrrwg1gwwbobo1b'    
+            expectedResult = 'w'             
+
+            actualResult = solveWhiteCross(inputDict)[0]
+            self.assertEqual(expectedResult, actualResult[2][1])
+            self.assertEqual(expectedResult, actualResult[2][3])
+            self.assertEqual(expectedResult, actualResult[2][4])
+            self.assertEqual(expectedResult, actualResult[2][5])
+            self.assertEqual(expectedResult, actualResult[2][7])
+            
             
         
 
