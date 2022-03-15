2585ccdb16cff6bead5a8096c715f4d410989e97
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-15 10:20:48 -0500

    Red Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -88,7 +88,7 @@ class SolveWhiteCrossTest(unittest.TestCase):
             self.assertEqual(expectedResult, actualResult[4][5])
             self.assertEqual(expectedResult, actualResult[4][7])
             
-        def test_010_SolveWhiteCross_y0_NoYellow(self): #if yellow not exist, then [0][4] is yellow, opposite is white
+        def test_080_SolveWhiteCross_y0_NoYellow(self): #if yellow not exist, then [0][4] is yellow, opposite is white
             inputDict = {}
             inputDict['cube'] = 'r2rr1googg1r1b1o21bbbb2rro1oog2ggbr12o2grr2g1g22bobo1b'    
             expectedResult = '2'             
