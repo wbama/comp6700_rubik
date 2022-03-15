3792e0b738259ae28b31b57565ea2ea31f97fe45
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-15 09:14:43 -0500

    Red Light |

--- rubik/solve.py
+++ rubik/solve.py
@@ -63,21 +63,7 @@ def _solve(parms):
     
             #if all the sides only have one character, cube is solved, no solution
             if len(s0) == len(s1) == len(s2) == len(s3) == len(s4) == len(s5) == 1:
-                result['solution'] = "" 
-                
-        # if 'rotate' in parms and (parms.get('rotate')) == None:
-        #         c_rotate_cube = (rotateSide_F(lst_cube)) 
-        #         str1 = "".join(c_rotate_cube[0])
-        #         str2 = "".join(c_rotate_cube[1])
-        #         str3 = "".join(c_rotate_cube[2])
-        #         str4 = "".join(c_rotate_cube[3])
-        #         str5 = "".join(c_rotate_cube[4])
-        #         str6 = "".join(c_rotate_cube[5])                       
-        #
-        #         str_cube = str1+str2+str3+str4+str5+str6                    
-        #         result = {}
-        #         result['cube'] = str_cube
-        #         result['status'] = 'ok'                
+                result['solution'] = ""              
            
          
         if 'rotate' in parms and rotate_length > 0:
@@ -237,9 +223,6 @@ def _solve(parms):
     ######################################################
     # Cleanup
     ######################################################   
-    lst_cube = createCubeListFromInputParms(parms) 
-
-    
     
     #delete the cube string if giving solutions    
     if 'solution' in result:
