768481ec82c24295e7b97643f771cf932e8a8593
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-17 15:35:22 -0500

    Refactor |

--- rubik/solveWhiteCross.py
+++ rubik/solveWhiteCross.py
@@ -2,9 +2,9 @@
     Created on 03/05/2022
     @author: Waldo du Toit
     program for solving the white cross
+    03/17 - Refactoring. Replace i in range with _
     
 """
-
 
 from rubik.solveRotations import rotateIntoWhiteCross_y_0, rotateIntoWhiteCross_y_1, rotateIntoWhiteCross_y_2, rotateIntoWhiteCross_y_3
 from rubik.solveRotations import rotateIntoWhiteCross_y_4, rotateIntoWhiteCross_y_5
@@ -22,42 +22,42 @@ def solveWhiteCross(parms):
         lst_cube_from_daisy = ((rotateIntoWhiteCross_y_0(solveDaisy(parms)[0], solveDaisy(parms)[1])))[0]
         rotation_from_daisy = ((rotateIntoWhiteCross_y_0(solveDaisy(parms)[0], solveDaisy(parms)[1])))[1]        
         whiteCrossCube = (rotateIntoWhiteCross_y_0(lst_cube_from_daisy, rotation_from_daisy ))      
-        for i in range(20):
+        for _ in range(20):
             whiteCrossCube = (rotateIntoWhiteCross_y_0(whiteCrossCube[0], whiteCrossCube[1]))        
         
     elif createCubeListFromInputParms(parms)[1][4] == var_y: 
         lst_cube_from_daisy = ((rotateIntoWhiteCross_y_1(solveDaisy(parms)[0], solveDaisy(parms)[1])))[0]
         rotation_from_daisy = ((rotateIntoWhiteCross_y_1(solveDaisy(parms)[0], solveDaisy(parms)[1])))[1]        
         whiteCrossCube = (rotateIntoWhiteCross_y_1(lst_cube_from_daisy, rotation_from_daisy ))             
-        for i in range(20):
+        for _ in range(20):
             whiteCrossCube = (rotateIntoWhiteCross_y_1(whiteCrossCube[0], whiteCrossCube[1])) 
             
     elif createCubeListFromInputParms(parms)[2][4] == var_y: 
         lst_cube_from_daisy = ((rotateIntoWhiteCross_y_2(solveDaisy(parms)[0], solveDaisy(parms)[1])))[0]
         rotation_from_daisy = ((rotateIntoWhiteCross_y_2(solveDaisy(parms)[0], solveDaisy(parms)[1])))[1]        
         whiteCrossCube = (rotateIntoWhiteCross_y_2(lst_cube_from_daisy, rotation_from_daisy ))             
-        for i in range(20):
+        for _ in range(20):
             whiteCrossCube = (rotateIntoWhiteCross_y_2(whiteCrossCube[0], whiteCrossCube[1])) 
             
     elif createCubeListFromInputParms(parms)[3][4] == var_y: 
         lst_cube_from_daisy = ((rotateIntoWhiteCross_y_3(solveDaisy(parms)[0], solveDaisy(parms)[1])))[0]
         rotation_from_daisy = ((rotateIntoWhiteCross_y_3(solveDaisy(parms)[0], solveDaisy(parms)[1])))[1]        
         whiteCrossCube = (rotateIntoWhiteCross_y_3(lst_cube_from_daisy, rotation_from_daisy ))      
-        for i in range(20):
+        for _ in range(20):
             whiteCrossCube = (rotateIntoWhiteCross_y_3(whiteCrossCube[0], whiteCrossCube[1]))        
         
     elif createCubeListFromInputParms(parms)[4][4] == var_y: 
         lst_cube_from_daisy = ((rotateIntoWhiteCross_y_4(solveDaisy(parms)[0], solveDaisy(parms)[1])))[0]
         rotation_from_daisy = ((rotateIntoWhiteCross_y_4(solveDaisy(parms)[0], solveDaisy(parms)[1])))[1]        
         whiteCrossCube = (rotateIntoWhiteCross_y_4(lst_cube_from_daisy, rotation_from_daisy ))             
-        for i in range(20):
+        for _ in range(20):
             whiteCrossCube = (rotateIntoWhiteCross_y_4(whiteCrossCube[0], whiteCrossCube[1])) 
             
     elif createCubeListFromInputParms(parms)[5][4] == var_y: 
         lst_cube_from_daisy = ((rotateIntoWhiteCross_y_5(solveDaisy(parms)[0], solveDaisy(parms)[1])))[0]
         rotation_from_daisy = ((rotateIntoWhiteCross_y_5(solveDaisy(parms)[0], solveDaisy(parms)[1])))[1]        
         whiteCrossCube = (rotateIntoWhiteCross_y_5(lst_cube_from_daisy, rotation_from_daisy ))             
-        for i in range(20):
+        for _ in range(20):
             whiteCrossCube = (rotateIntoWhiteCross_y_5(whiteCrossCube[0], whiteCrossCube[1]))                  
     
     return whiteCrossCube 
