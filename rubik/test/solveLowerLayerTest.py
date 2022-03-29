d94ad87bd6041e20c4498782c25c64a6c057c4a1
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-29 18:12:23 -0500

    Red Light |

--- rubik/test/solveLowerLayerTest.py
+++ rubik/test/solveLowerLayerTest.py
@@ -12,7 +12,21 @@ from rubik.solveLowerLayer import solveLowerLayer
 #need to update this. this is not a white cross. Look at the sides too
 class SolveLowerLayerTest(unittest.TestCase):  
     def test_010_SolveLowerLayer(self):   
-        pass            
+        inputDict = {}
+        inputDict['cube'] = 'gwwboboyboygwbyyyrygwrrgwowggyogrowbrwrrygoogyorrwbbbb'    
+        expectedResult = 'w'             
+
+        actualResult = solveLowerLayer(inputDict)[0]
+        self.assertEqual(expectedResult, actualResult[5][1])
+        self.assertEqual(expectedResult, actualResult[5][3])
+        self.assertEqual(expectedResult, actualResult[5][4])
+        self.assertEqual(expectedResult, actualResult[5][5])
+        self.assertEqual(expectedResult, actualResult[5][7])
+        #sides should be same color as the middle cells
+        self.assertEqual(actualResult[3][7], actualResult[3][4])
+        self.assertEqual(actualResult[0][7], actualResult[0][4])
+        self.assertEqual(actualResult[1][7], actualResult[1][4])
+        self.assertEqual(actualResult[2][7], actualResult[2][4])            
 
 
             
