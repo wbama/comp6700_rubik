f7caa10648627d9e0cd1d3b184e9212cbc973442
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-03-29 18:23:15 -0500

    Red Light |

--- rubik/test/solveLowerLayerTest.py
+++ rubik/test/solveLowerLayerTest.py
@@ -29,7 +29,26 @@ class SolveLowerLayerTest(unittest.TestCase):
         self.assertEqual(expectedResult, actualResult[5][8])
         #sides should also be solved
         
+    def test_020_SolveLowerLayer(self):   
+        inputDict = {}
+        inputDict['cube'] = 'oyyobgwwrbbowrwwygbrwogorwwbrgyobrooogybyyyrrgrbgwggby'    
+        expectedResult = 'w'  
+                   
+
+        actualResult = solveLowerLayer(inputDict)[0]
+        print(actualResult)
+        self.assertEqual(expectedResult, actualResult[5][1])
+        self.assertEqual(expectedResult, actualResult[5][2])
+        self.assertEqual(expectedResult, actualResult[5][3])
+        self.assertEqual(expectedResult, actualResult[5][4])
+        self.assertEqual(expectedResult, actualResult[5][5])
+        self.assertEqual(expectedResult, actualResult[5][6])
+        self.assertEqual(expectedResult, actualResult[5][7])
+        self.assertEqual(expectedResult, actualResult[5][8])
+        #sides should also be solved
+        
           
+
 
 
             
