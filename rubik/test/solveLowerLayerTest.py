dcb26ad259248dc4b712f929ba7e64d7095f34ed
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-06 13:04:49 -0500

    Green Light |

--- rubik/test/solveLowerLayerTest.py
+++ rubik/test/solveLowerLayerTest.py
@@ -13,7 +13,7 @@ from rubik.solveLowerLayer import solveLowerLayer
 class SolveLowerLayerTest(unittest.TestCase):  
     def test_010_SolveLowerLayer(self):   
         inputDict = {}
-        inputDict['cube'] = 'WvvvvWEvqWqhEWhEWAAEhWhhqhEWvAqqWqqWAhqqEEvEEhAvAAAhAv'  
+        inputDict['cube'] = 'O7xzBOBBzzzB77OO7BOBOzzO7z77O7BxxzxxzxxxO7BB999x999999'  
         var_w = inputDict['cube'][-5]
         expectedResult_col = var_w 
                    
