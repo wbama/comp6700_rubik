88b9fb0c8bccf276824dd99de1ab2323a144136b
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-06 14:39:02 -0500

    Green Light |

--- rubik/test/solveWhiteCrossTest.py
+++ rubik/test/solveWhiteCrossTest.py
@@ -15,7 +15,7 @@ class SolveWhiteCrossTest(unittest.TestCase):
         def test_010_SolveWhiteCross(self):
             inputDict = {}
             #inputDict['cube'] = 'qBu1HqHH111HBBqqBHqHq11qB1BBqBHuu1uu1uuuqBHHOOOuOOOOOO'   
-            inputDict['cube'] = 'ybybgrbowboogoorbowywgbygrwoyggrrbgygbbryyrorowgwwwrwy'    
+            inputDict['cube'] = 'goobbybbowgrgrrgrggywygrogyrbwgorborbbyoyorybywywwwoww'    
             var_w = inputDict['cube'][-5]
             expectedResult = var_w       
 
