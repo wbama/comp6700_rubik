379d1a274ea007d278bdece36329c09043bb3a80
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-13 10:29:47 -0500

    Green Light |

--- rubik/test/solveMiddleLayerTest.py
+++ rubik/test/solveMiddleLayerTest.py
@@ -26,6 +26,28 @@ class SolveMiddleLayerTest(unittest.TestCase):
             self.assertEqual(actualResult[2][1], actualResult[2][4], actualResult[2][6]) 
         elif actualResult[2][1] == actualResult[2][4]:
             self.assertEqual(actualResult[3][1], actualResult[3][4], actualResult[3][6])
+            
+    def test_020_SolveMiddleLayer(self):   
+        inputDict = {}
+        inputDict['cube'] = 'SoYsssowwSww33YssSSwsSSo3Ys3ooSwo3SwosYwoYw33sSY3YYY3o'  
+        var_w = inputDict['cube'][-5]
+        expectedResult_col = var_w 
+                   
+
+        actualResult = solveLowerLayer(inputDict)[0]
+        self.assertEqual(expectedResult_col, actualResult[5][1])
+        self.assertEqual(expectedResult_col, actualResult[5][2])
+        self.assertEqual(expectedResult_col, actualResult[5][3])
+        self.assertEqual(expectedResult_col, actualResult[5][4])
+        self.assertEqual(expectedResult_col, actualResult[5][5])
+        self.assertEqual(expectedResult_col, actualResult[5][6])
+        self.assertEqual(expectedResult_col, actualResult[5][7])
+        self.assertEqual(expectedResult_col, actualResult[5][8])
+        #sides should also be solved
+        self.assertEqual(actualResult[0][6], actualResult[0][7], actualResult[0][7])
+        self.assertEqual(actualResult[1][6], actualResult[1][7], actualResult[1][7])
+        self.assertEqual(actualResult[2][6], actualResult[2][7], actualResult[2][7])
+        self.assertEqual(actualResult[3][6], actualResult[3][7], actualResult[3][7])
 
         
         
