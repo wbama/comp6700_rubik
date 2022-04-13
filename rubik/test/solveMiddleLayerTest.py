9413b1b7babaecc1d567c293086c76feb21ff3f9
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-13 10:10:48 -0500

    Green Light |

--- rubik/test/solveMiddleLayerTest.py
+++ rubik/test/solveMiddleLayerTest.py
@@ -54,9 +54,12 @@ class SolveLowerLayerTest(unittest.TestCase):
         #sides should also be solved
         if actualResult[0][1] == actualResult[0][4]:
             self.assertEqual(actualResult[0][1], actualResult[0][4], actualResult[0][6]) 
-        self.assertEqual(actualResult[1][1], actualResult[1][4], actualResult[1][6]) 
-        # self.assertEqual(actualResult[2][1], actualResult[2][4], actualResult[2][6]) 
-        # self.assertEqual(actualResult[3][1], actualResult[3][4], actualResult[3][6])
+        elif actualResult[1][1] == actualResult[1][4]:
+            self.assertEqual(actualResult[1][1], actualResult[1][4], actualResult[1][6]) 
+        elif actualResult[2][1] == actualResult[2][4]:
+            self.assertEqual(actualResult[2][1], actualResult[2][4], actualResult[2][6]) 
+        elif actualResult[2][1] == actualResult[2][4]:
+            self.assertEqual(actualResult[3][1], actualResult[3][4], actualResult[3][6])
 
         
         
