f16f1c639851e74c6f332cf8609f151f51315748
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-14 12:23:55 -0500

    Green Light |

--- rubik/solveMiddleLayer.py
+++ rubik/solveMiddleLayer.py
@@ -12,6 +12,7 @@ from rubik.solveLowerLayer import solveLowerLayer
 
 def solveMiddleLayer(parms):
     
+    var_y = createYellowAndWhiteVariables(parms)[0]
     var_w = createYellowAndWhiteVariables(parms)[1]
     #the input cube with the white cross    
     lst_cube = solveLowerLayer(parms)[0]    
@@ -29,16 +30,27 @@ def solveMiddleLayer(parms):
                 # print("top w corners")
                 lst_top_w_corners = solve_top_w_corners(lst_cube, lst_rotate)
                 lst_cube = lst_top_w_corners[0]
-                lst_rotate = lst_top_w_corners[1]
-                
+                lst_rotate = lst_top_w_corners[1] 
+                            
         if lst_cube[5][0] == var_w and lst_cube[5][1] == var_w and lst_cube[5][2] == var_w and lst_cube[5][3] == var_w and \
-        lst_cube[5][4] == var_w and lst_cube[5][5] == var_w and lst_cube[5][6] == var_w and lst_cube[5][7] == var_w and \
-        lst_cube[5][8] == var_w and \
-        (lst_cube[0][3] == lst_cube[0][4] == lst_cube[0][5] == lst_cube[0][6] == lst_cube[0][7] == lst_cube[0][8]) and \
-        (lst_cube[1][3] == lst_cube[1][4] == lst_cube[1][5] == lst_cube[1][6] == lst_cube[1][7] == lst_cube[1][8]) and \
-        (lst_cube[2][3] == lst_cube[2][4] == lst_cube[2][5] == lst_cube[2][6] == lst_cube[2][7] == lst_cube[2][8]) and \
-        (lst_cube[3][3] == lst_cube[3][4] == lst_cube[3][5] == lst_cube[3][6] == lst_cube[3][7] == lst_cube[3][8]):
+            lst_cube[5][4] == var_w and lst_cube[5][5] == var_w and lst_cube[5][6] == var_w and lst_cube[5][7] == var_w and \
+            lst_cube[5][8] == var_w and \
+            (lst_cube[0][3] == lst_cube[0][4] == lst_cube[0][5] == lst_cube[0][6] == lst_cube[0][7] == lst_cube[0][8]) and \
+            (lst_cube[1][3] == lst_cube[1][4] == lst_cube[1][5] == lst_cube[1][6] == lst_cube[1][7] == lst_cube[1][8]) and \
+            (lst_cube[2][3] == lst_cube[2][4] == lst_cube[2][5] == lst_cube[2][6] == lst_cube[2][7] == lst_cube[2][8]) and \
+            (lst_cube[3][3] == lst_cube[3][4] == lst_cube[3][5] == lst_cube[3][6] == lst_cube[3][7] == lst_cube[3][8]):
             break
+        
+        #no edge pieces without yellow, but middle layer not solved. If middle layer solved, would break
+        if (lst_cube[4][0] == var_y or lst_cube[3][0]  == var_y or lst_cube[2][2]  == var_y) and \
+            (lst_cube[4][1] == var_y or lst_cube[2][1]  == var_y) and \
+            (lst_cube[4][2] == var_y or lst_cube[2][0]  == var_y or lst_cube[1][2]  == var_y) and \
+            (lst_cube[4][3] == var_y or lst_cube[3][1] == var_y) and \
+            (lst_cube[4][5] == var_y or lst_cube[1][1] == var_y ) and \
+            (lst_cube[4][6] == var_y or lst_cube[0][0]  == var_y or lst_cube[3][2]  == var_y) and \
+            (lst_cube[4][7] == var_y or lst_cube[0][1] == var_y ) and \
+            (lst_cube[4][8] == var_y or lst_cube[0][2]  == var_y or lst_cube[3][0]  == var_y):
+            print("edge piece not yellow")
     
     return lst_cube, lst_rotate
 
