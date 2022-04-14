4e13d6e5844bd13f4db503ba47655fe47d6f707d
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-14 13:29:31 -0500

    Green Light |

--- rubik/test/solveTest.py
+++ rubik/test/solveTest.py
@@ -264,6 +264,36 @@ class SolveTest(unittest.TestCase):
         self.assertEqual( (actualResult.get('cube'))[15],  (actualResult.get('cube'))[16],  (actualResult.get('cube'))[17])
         self.assertEqual( (actualResult.get('cube'))[24],  (actualResult.get('cube'))[25],  (actualResult.get('cube'))[26])
         self.assertEqual( (actualResult.get('cube'))[33],  (actualResult.get('cube'))[34],  (actualResult.get('cube'))[35])
+        
+    def test_170_SolveMiddleLayer(self):   
+        inputDict = {}
+        #inputDict['cube'] = 'SoYsssowwSww33YssSSwsSSo3Ys3ooSwo3SwosYwoYw33sSY3YYY3o'  
+        inputDict['cube'] = 'SoYsssowwSww33YssSSwsSSo3Ys3ooSwo3SwosYwoYw33sSY3YYY3o'
+        var_w = inputDict['cube'][-5]
+        expectedResult_col = var_w 
+                   
+
+        actualResult = solve._solve(inputDict)
+        self.assertEqual(expectedResult_col, actualResult[5][1])
+        self.assertEqual(expectedResult_col, actualResult[5][2])
+        self.assertEqual(expectedResult_col, actualResult[5][3])
+        self.assertEqual(expectedResult_col, actualResult[5][4])
+        self.assertEqual(expectedResult_col, actualResult[5][5])
+        self.assertEqual(expectedResult_col, actualResult[5][6])
+        self.assertEqual(expectedResult_col, actualResult[5][7])
+        self.assertEqual(expectedResult_col, actualResult[5][8])
+        #sides should also be solved
+        self.assertEqual(actualResult[0][3], actualResult[0][4], actualResult[0][5])
+        self.assertEqual(actualResult[0][6], actualResult[0][7], actualResult[0][7])
+        
+        self.assertEqual(actualResult[1][3], actualResult[1][4], actualResult[1][5])
+        self.assertEqual(actualResult[1][6], actualResult[1][7], actualResult[1][7])
+        
+        self.assertEqual(actualResult[2][3], actualResult[2][4], actualResult[2][5])
+        self.assertEqual(actualResult[2][6], actualResult[2][7], actualResult[2][7])
+        
+        self.assertEqual(actualResult[3][3], actualResult[3][4], actualResult[3][5])
+        self.assertEqual(actualResult[3][6], actualResult[3][7], actualResult[3][7])
 
 
 # Sad path tests
