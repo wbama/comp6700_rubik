e559212339bb3a53cbff8d6950774185ec341452
P15s_PC <P15s_PC@LAPTOP-V13746S9>
2022-04-15 07:51:50 -0500

    Green Light |

--- rubik/test/solveMiddleLayerTest.py
+++ rubik/test/solveMiddleLayerTest.py
@@ -14,7 +14,7 @@ class SolveMiddleLayerTest(unittest.TestCase):
     def test_010_SolveMiddleLayer(self):   
         inputDict = {}
         #inputDict['cube'] = 'SoYsssowwSww33YssSSwsSSo3Ys3ooSwo3SwosYwoYw33sSY3YYY3o'  
-        inputDict['cube'] = 'oyyrobgobbgrobowbbgyygryrrrggyogbggworyyyrbbrowowwwwww'
+        inputDict['cube'] = 'L52255e5eLL2Le22ekkekek5wkewk52LkwL2LkLL22we55wkwww5we'
         var_w = inputDict['cube'][-5]
         expectedResult_col = var_w 
                    
@@ -40,9 +40,36 @@ class SolveMiddleLayerTest(unittest.TestCase):
         
         self.assertEqual(actualResult[3][3], actualResult[3][4], actualResult[3][5])
         self.assertEqual(actualResult[3][6], actualResult[3][7], actualResult[3][7])
+        
+    def test_020_SolveMiddleLayer(self):   
+        inputDict = {}
+        #inputDict['cube'] = 'SoYsssowwSww33YssSSwsSSo3Ys3ooSwo3SwosYwoYw33sSY3YYY3o'  
+        inputDict['cube'] = 'oyyrobgobbgrobowbbgyygryrrrggyogbggworyyyrbbrowowwwwww'
+        var_w = inputDict['cube'][-5]
+        expectedResult_col = var_w 
+                   
 
+        actualResult = solveMiddleLayer(inputDict)[0]
+        self.assertEqual(expectedResult_col, actualResult[5][1])
+        self.assertEqual(expectedResult_col, actualResult[5][2])
+        self.assertEqual(expectedResult_col, actualResult[5][3])
+        self.assertEqual(expectedResult_col, actualResult[5][4])
+        self.assertEqual(expectedResult_col, actualResult[5][5])
+        self.assertEqual(expectedResult_col, actualResult[5][6])
+        self.assertEqual(expectedResult_col, actualResult[5][7])
+        self.assertEqual(expectedResult_col, actualResult[5][8])
+        #sides should also be solved
+        self.assertEqual(actualResult[0][3], actualResult[0][4], actualResult[0][5])
+        self.assertEqual(actualResult[0][6], actualResult[0][7], actualResult[0][7])
         
+        self.assertEqual(actualResult[1][3], actualResult[1][4], actualResult[1][5])
+        self.assertEqual(actualResult[1][6], actualResult[1][7], actualResult[1][7])
         
+        self.assertEqual(actualResult[2][3], actualResult[2][4], actualResult[2][5])
+        self.assertEqual(actualResult[2][6], actualResult[2][7], actualResult[2][7])
+        
+        self.assertEqual(actualResult[3][3], actualResult[3][4], actualResult[3][5])
+        self.assertEqual(actualResult[3][6], actualResult[3][7], actualResult[3][7])        
           
 
 
